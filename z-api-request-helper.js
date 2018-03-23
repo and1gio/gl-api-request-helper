2dfc8d7655b551ec36e5b2c3f6dc67228e53ef3b
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-03-23 16:59:57 +0400

    fix

--- z-api-request-helper.js
+++ z-api-request-helper.js
@@ -1,22 +1,26 @@
-var request = require('request');
+const request = require('request');
 
-var ZApiRequestHelper = function (config, logger) {
-    if(!config) {
+let ZApiRequestHelper = function (config, logger) {
+    if (!config) {
         throw { keyword: 'API_REQUEST_HELPER_CONFIG_MISSING' };
     }
     this.config = config;
     this.logger = logger;
 };
 
-var zApiRequestHelper = ZApiRequestHelper.prototype;
+let zApiRequestHelper = ZApiRequestHelper.prototype;
 
-zApiRequestHelper.makeRequest = function(type, method, params, cb) {
+zApiRequestHelper.makeRequest = function (type, method, params, cb) {
+    let me = this;
 
-    var me = this;
-    var config = me.config;
-    var apiUrl = "http://" + config.host + ":" + config.port + config.path + method;
+    const protocol = me.config.protocol || "http://";
+    const host = me.config.host;
+    const port = me.config.port;
+    const path = me.config.path;
 
-    if (config && config.debug) {
+    const apiUrl = protocol.concat(host, ":", port, path, method);
+
+    if (me.config.debug) {
         if (me.logger) {
             me.logger.info(type, apiUrl, params);
         } else {
@@ -24,73 +28,73 @@ zApiRequestHelper.makeRequest = function(type, method, params, cb) {
         }
     }
 
-    var params = {url: apiUrl, json: params};
+    params = { url: apiUrl, json: params };
 
-    if(config.timeout){
-        params.timeout = config.timeout
+    if (me.config.timeout) {
+        params.timeout = me.config.timeout
     }
 
     if (type !== 'get') {
-        request[type](params, function (error, response, body) {
-            me.handleSendResponse(error, response, body, cb);
+        request[type](params, (error, response, body) => {
+            handleSendResponse(error, response, body, cb);
         });
     } else {
-        request(apiUrl, function (error, response, body) {
-            me.handleSendResponse(error, response, body, cb);
-        })
+        request(apiUrl, (error, response, body) => {
+            handleSendResponse(error, response, body, cb);
+        });
     }
-};
-
-zApiRequestHelper.handleSendResponse = function (fnError, response, body, callback) {
-    if (fnError) {
-        return callback([{keyword: 'CONNECTION_ERROR', error: fnError}], null);
-    }
-
-    if (!body) {
-        return callback([{keyword: 'RESPONSE_BODY_IS_EMPTY'}], null);
-    }
-
-    if (body.error) {
-        return callback(body.error, null);
-    }
-
-    callback(null, body.result);
 };
 
 zApiRequestHelper.get = function (method, cb) {
-    var me = this;
-    me.makeRequest('get', method, null, function (err, res) {
+    let me = this;
+    me.makeRequest('get', method, null, (err, res) => {
         return cb(err, res);
     });
 };
 
 zApiRequestHelper.post = function (method, params, cb) {
-    var me = this;
-    me.makeRequest('post', method, params, function (err, res) {
+    let me = this;
+    me.makeRequest('post', method, params, (err, res) => {
         return cb(err, res);
     });
 };
 
 zApiRequestHelper.put = function (method, params, cb) {
-    var me = this;
-    me.makeRequest('put', method, params, function (err, res) {
+    let me = this;
+    me.makeRequest('put', method, params, (err, res) => {
         return cb(err, res);
     });
 };
 
 zApiRequestHelper.delete = function (method, params, cb) {
-    var me = this;
-    me.makeRequest('delete', method, params, function (err, res) {
+    let me = this;
+    me.makeRequest('delete', method, params, (err, res) => {
         return cb(err, res);
     });
 };
 
 // TODO for msda - deprecated
 zApiRequestHelper.request = function (method, params, cb) {
-    var me = this;
-    me.makeRequest('post', method, params, function (err, res) {
+    let me = this;
+    me.makeRequest('post', method, params, (err, res) => {
         return cb(err, res);
     });
+};
+
+function handleSendResponse(fnError, response, body, callback) {
+    if (fnError) {
+        return callback([{ keyword: 'CONNECTION_ERROR', error: fnError }], null);
+    }
+
+    if (!body) {
+        return callback([{ keyword: 'RESPONSE_BODY_IS_EMPTY' }], null);
+    }
+
+    if (body.error) {
+        return callback(body.error, null);
+    }
+
+    callback(null, body.result || body);
 };
 
 module.exports = ZApiRequestHelper;
